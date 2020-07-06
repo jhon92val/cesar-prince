20cdea77ff25cc23439721c5f7180cd89356ceed
Jhonathan Perdomo <jhon92val@gmail.com>
2020-07-06 12:55:15 -0400

    Parallax JS, Barra de busqueda animada

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-// object-fit polyfill run
-objectFitImages();
+$(document).ready(function(){
+ 
+    var ancho = $(window).width();
+ 
+    // Efecto parallax en imagen de inicio;
+        if (ancho <= 1350){
+            $('header').css({
+                'background-size': 'initial'
+            });
+        }
 
-/* init Jarallax */
-jarallax(document.querySelectorAll('.jarallax'));
-
-jarallax(document.querySelectorAll('.jarallax-keep-img'), {
-    keepImg: true,
+	$(window).scroll(function(){
+		var barra = $(window).scrollTop();
+		var posicion =  (barra * 0.30);
+		
+		$('header').css({
+			'background-position': '0 -' + posicion + 'px'
+        });
+        
+	});
+ 
 });
+
