88b3d8575bd93e7e8d23aabffc4fafdba144a899
Jhonathan Perdomo <jhon92val@gmail.com>
2020-07-07 16:05:09 -0400

    Seccion Nosotros y contenido debajo

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
 	$(window).scroll(function(){
 		var barra = $(window).scrollTop();
-		var posicion =  (barra * 0.30);
+		var posicion =  (barra * 0.50);
 		
 		$('header').css({
 			'background-position': '0 -' + posicion + 'px'
